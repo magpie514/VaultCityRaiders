21585c6c59b95a925cf667659bdb0ce72a35215b
Magpie <magpie514@users.noreply.github.com>
2019-05-16 19:34:18 +0200

    Some prep work and notes for available races and classes.

--- classes/library/lib_class.gd
+++ classes/library/lib_class.gd
@@ -14,23 +14,25 @@ var example = {
 				["debug", "debugi2"],
 			],
 		},
-		"ofight" : {
+		"ofight" : { #Jay Hawking's story mode base class.
 			name = "Orbital Fighter",
 			description = "A technical class specializing in exploiting enemy actions and status.",
 			statSpread = [ [005, 000, 000, 003, 002, 004, 001], [032, 000, 006, 032, 024, 052, 018] ],
 			weapons = [skill.WPCLASS_ARTILLERY, skill.WPCLASS_FIREARM, skill.WPCLASS_SHIELD, skill.WPCLASS_LONGSWORD],
 			skills = [
 				["story", "plasfeld"],
-				["story", "thunswrd"],
+				["story", "thunswrd"], #Move to ORBITAL Cannon.
+				# Core skills #########
 				["debug", "restshrd"],
 				["story", "freerang"],
+				#Filler
 				["debug", "gmissile"],
 				["debug", "gatebrkr"],
 				["debug", "codexalt"],
 				["debug", "blddance"],
 			],
 		},
-		"gdriver" : {
+		"gdriver" : { #0a0a-DT-KSSG "Magpie" Miller's story mode base class.
 			name = "G-Driver",
 			description = "A technical class specializing in exploiting enemy actions and status.",
 			statSpread = [ [004, 000, 000, 003, 002, 003, 001], [032, 000, 006, 032, 024, 040, 018] ],
@@ -43,14 +45,14 @@ var example = {
 				["debug", "restshrd"],
 				["story", "gravrefl"],
 				["story", "gmissile"],
-				["story", "gatebrkr"],
-				["story", "codexalt"],
+				["story", "gatebrkr"], #Make an Over skill?
+				["story", "codexalt"], #Make an Over skill?
 				# Glaive/Auger skills #
-				["story", "gemshrap"],
+				["story", "gemshrap"], #Move to Boost Glaive.
 				["story", "spirbost"],
 			],
 		},
-		"akashic" : {
+		"akashic" : { #Aohana Yukiko's story mode base class.
 			name = "Akashic Knight",
 			description = "A technical class specializing in exploiting enemy actions and status.",
 			statSpread = [ [004, 000, 000, 003, 002, 003, 001], [032, 000, 006, 032, 024, 040, 018] ],
@@ -65,36 +67,82 @@ var example = {
 				["debug", "kamaita"],
 			],
 		},
-		"muramasa" : {
+		"muramasa" : { #Kurohara Shiro's story mode base class.
 			name = "Muramasa",
 			description = "A defensive class specializing in high defensive maneuvers, counters, and ghostly abilities",
 			statSpread = [ [004, 000, 000, 003, 002, 003, 001], [032, 000, 006, 032, 024, 040, 018] ],
 			weapons = [skill.WPCLASS_SHIELD, skill.WPCLASS_SHORTSWORD, skill.WPCLASS_LONGSWORD],
 			skills = [
+				#Needs huge rework. I need a Japanese dictionary pronto.
 				["debug", "defdown"],
 				["debug", "barricad"],
 				["debug", "solidbun"],
 				["debug", "decoy"],
 			],
 		},
+		"incinera" : { #Anna Westenra's story mode base class.
+			name = "Incinerator",
+			description = "Brutal fighter using the power of flames and time.",
+			weapons = [skill.WPCLASS_ARTILLERY, skill.WPCLASS_SHORTSWORD, skill.WPCLASS_POLEARM],
+			statSpread = [ [004, 004, 001, 002, 001, 000, 002], [040, 048, 008, 022, 008, 008, 024] ],
+			skills = [
+				#Define Anna's roles better.
+				["debug", "vampdran"],
+				["debug", "overclck"],
+				["debug", "lunablaz"],
+			],
+		}
 		"esper" : {
 			name = "ESPer",
-			description = "A balanced fighter with powerful psychic abilities.",
+			description = "A physically weak fighter but with powerful psychic abilities. Their physical fragility makes them better on the back row.",
 			weapons = [skill.WPCLASS_HANDGUN, skill.WPCLASS_SHIELD, skill.WPCLASS_SHORTSWORD],
 			statSpread = [ [002, 000, 000, 003, 000, 002, 003], [018, 002, 002, 048, 024, 022, 028] ],
 			skills = [
 				["debug", "illusion"],
-				["debug", "rosegrdn"],
 			],
 		},
-		"defender" : {
-			name = "Defender",
-			description = "A highly durable fighter capable of withstanding the mightiest of blows.",
+		"riot" : {
+			name = "Riot",
+			description = "A heavily defensive fighter with heavy armor and defenses, ideal for the front lines, but their skill with firearms makes them usable at the back, too.",
 			weapons = [skill.WPCLASS_FIREARM, skill.WPCLASS_SHIELD, skill.WPCLASS_HAMMER],
 			statSpread = [ [005, 002, 005, 000, 001, 000, 002], [050, 020, 048, 000, 012, 004, 016] ],
 			skills = [
 				["debug", "barricad"],
 				["debug", "solidbun"],
+			],
+		},
+		"idol" : {
+			name = "Idol",
+			description = "Support class designed to assist the whole team. The agility required for their cheerful dances can allow them to be efficient fighters, too.",
+			weapons = [skill.WPCLASS_FIREARM, skill.WPCLASS_SHIELD, skill.WPCLASS_HAMMER],
+			statSpread = [ [005, 002, 005, 000, 001, 000, 002], [050, 020, 048, 000, 012, 004, 016] ],
+			skills = [
+				["debug", "speedup"],
+			],
+		},
+		"fengshui" : {
+			name = "Feng-Shui",
+			description = "Adept fighters that can utilize environmental energies for a variety of skills. They are usually better suited for the back row.",
+			weapons = [skill.WPCLASS_FIREARM, skill.WPCLASS_SHIELD, skill.WPCLASS_HAMMER],
+			statSpread = [ [005, 002, 005, 000, 001, 000, 002], [050, 020, 048, 000, 012, 004, 016] ],
+			skills = [
+				["debug", "elemshot"],
+			],
+		},
+		"rider" : {
+			name = "Rider",
+			description = "Adventurers that compensate their lack of combat skills with various machines to ride. While their growth is fully dependent on their ride, that makes them highly versatile.",
+			weapons = [skill.WPCLASS_FIREARM, skill.WPCLASS_SHIELD, skill.WPCLASS_HAMMER],
+			statSpread = [ [005, 002, 005, 000, 001, 000, 002], [050, 020, 048, 000, 012, 004, 016] ],
+			skills = [
+			],
+		},
+		"salarymn" : {
+			name = "Salaryman",
+			description = "Untrained adventurers unsatisfied with their office jobs. However, the spirit of adventure burns strong within them, and are full of potential.",
+			weapons = [skill.WPCLASS_FIREARM, skill.WPCLASS_SHIELD, skill.WPCLASS_HAMMER],
+			statSpread = [ [005, 002, 005, 000, 001, 000, 002], [050, 020, 048, 000, 012, 004, 016] ],
+			skills = [
 			],
 		},
 		"medic" : {
@@ -108,17 +156,6 @@ var example = {
 				["debug", "prtyheal"],
 			],
 		},
-		"incinera" : {
-			name = "Incinerator",
-			description = "Brutal fighter using the power of flames and time.",
-			weapons = [skill.WPCLASS_ARTILLERY, skill.WPCLASS_SHORTSWORD, skill.WPCLASS_POLEARM],
-			statSpread = [ [004, 004, 001, 002, 001, 000, 002], [040, 048, 008, 022, 008, 008, 024] ],
-			skills = [
-				["debug", "vampdran"],
-				["debug", "overclck"],
-				["debug", "lunablaz"],
-			],
-		}
 	}
 }
 
--- classes/library/lib_race.gd
+++ classes/library/lib_race.gd
@@ -11,12 +11,21 @@ var example = {
 			aspect = skill.RACEF_MEC,
 			race = [ skill.RACE_MACHINE ],
 		},
-		"human" : {
+		
+
+		"human" : { #Humans should be able to change races to cyborg naturally during the game.
 			name = "Human",
 			description = "The third of the three races engineered by Tiamat, the Originator. They have an inherent affinity to technology.",
 			statSpread = [ [045, 011, 013, 011, 010, 013, 014], [460, 120, 135, 100, 090, 130, 150] ],
 			aspect = skill.RACEF_BIO|skill.RACEF_SPI,
 			race = [ skill.RACE_HUMAN ],
+		},
+		"cyborg" : {
+			name = "Cyborg",
+			description = "A human partially augmented by technology. They are limited by their human bodies, but their augments allow for higher potential.",
+			statSpread = [ [035, 010, 012, 010, 008, 012, 013], [380, 100, 100, 100, 075, 125, 120] ],
+			aspect = skill.RACEF_BIO|skill.RACEF_SPI|skill.RACEF_MEC,
+			flags = [ skill.RACE_HUMAN, skill.RACE_MACHINE ],
 		},
 		"elf" : {
 			name = "Elf",
@@ -39,33 +48,58 @@ var example = {
 			aspect = skill.RACEF_BIO|skill.RACEF_SPI,
 			race = [ skill.RACE_HUMAN, skill.RACE_UNDEAD ],
 		},
-		"cyborg" : {
-			name = "Cyborg",
-			description = "A human partially augmented by technology. They are limited by their human bodies, but their augments allow for higher potential.",
-			statSpread = [ [035, 010, 012, 010, 008, 012, 013], [380, 100, 100, 100, 075, 125, 120] ],
-			aspect = skill.RACEF_BIO|skill.RACEF_SPI|skill.RACEF_MEC,
-			flags = [ skill.RACE_HUMAN, skill.RACE_MACHINE ],
-		},
-		"choujin" : {
-			name = "Choujin",
-			description = "One of the potential ultimate forms of humanity. A human soul in a full machine body.",
+		"robot" : {
+			name = "Robot",
+			description = "A race created by humans. They AI and physical capabilities are highly variable, but their potential is tied to their equipment.",
 			statSpread = [ [060, 015, 015, 015, 010, 010, 005], [520, 150, 165, 120, 090, 110, 085] ],
 			aspect = skill.RACEF_SPI|skill.RACEF_MEC,
 			flags = [ skill.RACE_MACHINE ],
 		},
-		"fairy" : {
+		"choujin" : { #Mostly story mode exclusive. Could probably look into making it a stronger but less versatile version of robots, like you get some initial picks and you get stuck with them.
+			name = "Choujin",
+			description = "One of the potential ultimate forms of humanity. A human soul in a full machine body. Being a type of robot, their potential is also tied to their equipment",
+			statSpread = [ [060, 015, 015, 015, 010, 010, 005], [520, 150, 165, 120, 090, 110, 085] ],
+			aspect = skill.RACEF_SPI|skill.RACEF_MEC,
+			player = false,
+			flags = [ skill.RACE_MACHINE ],
+		},
+		"fairy" : {  #The best class for energy attackers.
 			name = "Fairy",
 			description = "The second of the three races engineered by Tiamat, the Originator. They have an inherent affinity to nature and the elements.",
 			statSpread = [ [040, 008, 008, 014, 014, 013, 010], [415, 090, 085, 150, 150, 130, 100] ],
 			aspect = skill.RACEF_BIO|skill.RACEF_SPI,
 			flags = [ skill.RACE_FAIRY ],
 		},
-		"dragon" : {
-			name = "Dragon",
-			description = "The first of the three races engineered by Tiamat, the Originator. They have an inherent affinity to power. They can change to a human form when needed.",
+		"dracon" : {  #I need a good name for this.
+			name = "??????", #This race should be the regular player-usable instance of dragon and act like Breath of Fire or something.
+			description = "A human with dragon blood, which raises their affinity to power. They can change into a dragon form.",
 			statSpread = [ [065, 014, 011, 014, 012, 013, 005], [600, 145, 120, 145, 130, 135, 050] ],
 			aspect = skill.RACEF_BIO|skill.RACEF_SPI,
+			flags = [ skill.RACE_HUMAN, skill.RACE_DRAGON ],
+		},
+		"dragon" : {  #This race I don't know what to do with. Dragons are supposed to be ridiculously powerful in this setting.
+			name = "Dragon",
+			description = "The first of the three races engineered by Tiamat, the Originator, and the closest to her heart. They have an inherent affinity to power.",
+			statSpread = [ [065, 014, 011, 014, 012, 013, 005], [600, 145, 120, 145, 130, 135, 050] ],
+			aspect = skill.RACEF_BIO|skill.RACEF_SPI,
+			player = false,
 			flags = [ skill.RACE_DRAGON ],
+		},
+		"god" : {     #Not available on player creation. I think. Maybe unlockable or cheaty?
+			name = "God",  #This is just some fantasy bull and not meant to represent anyone's view of theology.
+			description = "Higher beings born from the power of belief. Their power is tied to the prayer of their believers.",
+			statSpread = [ [065, 014, 011, 014, 012, 013, 005], [600, 145, 120, 145, 130, 135, 050] ],
+			aspect = skill.RACEF_SPI,
+			player = false,
+			flags = [ skill.RACE_GOD ],
+		},
+		"origin" : {  #Only for Tiamat and Cromwell.
+			name = "Originator",
+			description = "Born from the primordial chaos, Originators are gifted with the power to create universes. Only two are known to exist.",
+			statSpread = [ [065, 014, 011, 014, 012, 013, 005], [600, 145, 120, 145, 130, 135, 050] ],
+			aspect = skill.RACEF_SPI,
+			player = false,
+			flags = [ skill.RACE_ORIGINATOR ],
 		},
 	}
 }
@@ -76,6 +110,8 @@ func initTemplate():
 		"description": { loader = LIBSTD_STRING },
 		"statSpread": { loader = LIBSTD_STATSPREAD },
 		"aspect" : { loader = LIBSTD_INT },
+		"player" : { loader = LIBSTD_BOOL, default = true },
+		"skills" : { loader = LIBSTD_SKILL_LIST },
 	}
 
 func loadDebug():
@@ -110,3 +146,35 @@ func printData():
 					entry.statSpread[0][6], entry.statSpread[1][6],
 					entry.description,
 			  ])
+
+
+
+# Main objective
+# Core races should be generally suited for urban fantasy scenarios. Shadowrun but more anime, basically.
+# Due to the multiversal nature of the Vaults and the various worlds in "Creation" (capitalized),
+# technically anything goes, but the "core" world, Origin, is meant to be pretty advanced with
+# scifi and fantasy elements. Basically an alternate version of Earth where legends and heroes are a thing.
+# So yes you can technically have a French elf blasting Lemurian robots in the nega-verse or something.
+
+
+# In this setting, the Originator Tiamat engineered three base races with higher conscience.
+# Dragons, the closest to her, inherited the ability to manipulate "power", being able to absorb or
+# transfer "things" like strength, courage, elemental energies and whatever.
+# 	They are basically OP and will ruin your day. Do not piss them off.
+# Faeries came second, inherited the ability to interact with the forces of nature, astral bodies,
+# and the contents of the universe at large.
+# 	They are more balanced but can also ruin your day. Do not piss them off.
+# And last came humans, with the ability to create their own power.
+# 	They are the flimsiest ones but it's generally advised to not piss them off too much.
+# All other races came to be from natural causes but still following a link to her imagination.
+# If she imagines dog, dog will happen somewhere.
+# And then there's the Eldritch which are created naturally by the primordial chaos, but aren't
+# proper beings, and they can be pretty much anything. Absolute wildcards.
+#		Pissing them off could mean nothing, a lucky day, or the eradication of all sentient life.
+
+# Tiamat mostly leaves the races alone to evolve free, but all worlds contain or have
+# contained dragons, faeries and humans at the very least. Or have "attempted" to. Within these rules
+# a place like real life Earth would be one where only humans managed to thrive due to whatever conditions
+# with no dragons or fairies for magic to be a thing, but they still manifest as legends.
+# Thus xenos and other "not rubber forehead" alien worlds would be the work of the Eldritch and it's
+# an absolute ball of whatever in that case.
